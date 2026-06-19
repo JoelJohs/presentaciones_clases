c716756125c870a33902bdb157e0e3dfb011e339
Joel Josafat Hernandez Saucedo <jojohersa21@gmail.com>
2026-06-19 01:17:47 -0600

    feat: define content collection schema and add basic mock content

---
+++ src/content.config.ts
@@ -0,0 +1,13 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const lecciones = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/lecciones" }),
+  schema: z.object({
+    title: z.string(),
+    moduleTitle: z.string().optional(),
+    topicTitle: z.string().optional(),
+  }),
+});
+
+export const collections = { lecciones };
